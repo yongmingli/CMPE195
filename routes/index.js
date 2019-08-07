a825aa4b26dc2a38b7e281420c40fd96c285fc12
YongmingLi <yongming.li@sjsu.edu>
2019-08-06 17:39:27 -0700

    folder update

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ var passport = require('passport');
 /* Load other routes */
 router.use('/signup', require('./signup.js'));
 router.use('/login', require('./login.js'));
+router.use('/logout', require('./logout.js'));
 
 /* Routes */
 router.get('/', function(req, res, next) {
