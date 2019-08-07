5bc9697d40bf42bef39cb249aec0d174f05f1c02
YongmingLi <yongming.li@sjsu.edu>
2019-08-07 09:35:08 -0700

    updates

--- config/db_config.js
+++ config/db_config.js
@@ -1,6 +1,6 @@
 module.exports = {
     DB_USER: 'IsMondayTMR',
-    DB_PASSWORD: 'CMPE195!',
+    DB_PASSWORD: 'CMPE195',
     DB_HOST: 'cmpe195db-lki36.mongodb.net/test?retryWrites=true&w=majority',
     DB_NAME: 'CMPE195',
     COLLECTION_USERS: 'User'
