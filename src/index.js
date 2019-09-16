34f6d8d7a1a1fc8f39437f2e77482426b7a06664
chenghuiXue <chenghui.xue@sjsu.edu>
2019-09-15 21:18:34 -0700

    make src file clear
    move the login in file to one folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from'./App.js';
+import App from'./Login/App.js';
 
  
 ReactDOM.render(<App />, document.getElementById('root'));
