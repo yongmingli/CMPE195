9bc60fa24b9b2b4cdbf614851133f37caf5b5c2a
YongmingLi <yongming.li@sjsu.edu>
2019-10-13 16:08:46 -0700

    new
    Error: Cannot find module './logging/Logger'

---
+++ node_modules/@appbaseio/reactivemaps/lib/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+exports.__esModule = true;
+exports.ReactiveOpenStreetMap = exports.ReactiveGoogleMap = exports.GeoDistanceSlider = exports.GeoDistanceDropdown = undefined;
+
+var _ReactiveGoogleMap = require('./components/result/ReactiveGoogleMap');
+
+var _ReactiveGoogleMap2 = _interopRequireDefault(_ReactiveGoogleMap);
+
+var _ReactiveOpenStreetMap = require('./components/result/ReactiveOpenStreetMap');
+
+var _ReactiveOpenStreetMap2 = _interopRequireDefault(_ReactiveOpenStreetMap);
+
+var _GeoDistanceDropdown = require('./components/basic/GeoDistanceDropdown');
+
+var _GeoDistanceDropdown2 = _interopRequireDefault(_GeoDistanceDropdown);
+
+var _GeoDistanceSlider = require('./components/basic/GeoDistanceSlider');
+
+var _GeoDistanceSlider2 = _interopRequireDefault(_GeoDistanceSlider);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.GeoDistanceDropdown = _GeoDistanceDropdown2.default;
+exports.GeoDistanceSlider = _GeoDistanceSlider2.default;
+exports.ReactiveGoogleMap = _ReactiveGoogleMap2.default;
+exports.ReactiveOpenStreetMap = _ReactiveOpenStreetMap2.default;
---
+++ node_modules/@appbaseio/reactivesearch/lib/components/basic/ReactiveComponent.d.ts
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { CommonProps } from '../../';
+import * as types from '../../types';
+
+export interface ReactiveComponentProps {
+	children?: (...args: any[]) => any;
+	render?: (...args: any[]) => any;
+	componentId: string;
+	defaultQuery?: (...args: any[]) => any;
+	customQuery?: (...args: any[]) => any;
+	defaultValue?: any;
+	value?: any;
+	filterLabel?: string;
+	onData?: (...args: any[]) => any;
+	onError?: (...args: any[]) => any;
+	react?: types.react;
+	showFilter?: boolean;
+	onQueryChange?: (...args: any[]) => any;
+	URLParams?: boolean;
+}
+
+declare const ReactiveComponent: React.ComponentClass<ReactiveComponentProps>;
+
+export default ReactiveComponent;
